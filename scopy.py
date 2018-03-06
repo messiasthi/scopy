de59e3f74c122d4070d7db4dc6a62fd1331bf4b5
messiasthi <messiasthi@gmail.com>
2018-03-06 02:49:29 -0300

    Scopy added to git

---
+++ scopy.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python
+import sys
+import os
+import re
+
+params = [
+	'from',
+	'to',
+	'keywords',
+	'newwords',
+	'regex',
+	'uppercase',
+	'lowercase',
+	'bothcase'
+]
+
+def help():
+	print("The scopy is a alias to copy paste files")
+	print("with one difference, find and replace words or expressions.")
+	print("version: 1.0")
+	print("bug report/issues on https://github.com/messiasthi/scopy")
+	print("Usage:")
+	print("\tscopy from=\"/path/to/template.file\" to=\"/path/to/new.file\" keyword=\"example1\" replace=\"replace\"")
+	print("\t\tfrom\tThe original file")
+	print("\t\tto\tThe new file")
+	print("\t\tkeywords\tWords to replace for a new word")
+	print("\t\tnewwords\tNew words")
+
+def replace(line):
+	newLine = line
+	for word in words:
+		if word.lower() in newLine:
+			newLine = newLine.replace(word, replaces[words.index(word)].lower())
+		if word.upper() in newLine:
+			newLine = newLine.replace(word.upper(), replaces[words.index(word)].upper())
+		if word.title() in newLine:
+			newLine = newLine.replace(word.title(), replaces[words.index(word)].title())
+	return newLine
+
+
+
+originalFile=""
+newFile=""
+words=""
+replaces=""
+
+for value in sys.argv:
+	if "--help" in value or "-help" in value or "help" in value:
+		help()
+		exit(0)
+	elif "from=" in value:
+		originalFile = value.replace("from=", "")
+	elif "to=" in value:
+		newFile = value.replace("to=", "")
+	elif "keywords=" in value:
+		words = value.replace("keywords=", "").split()
+	elif "replaces=" in value:
+		replaces = value.replace("replaces=", "").split()
+
+if len(words) != len(replaces):
+	print("Error: The number of words and replaces must be equals")
+	exit(1)
+
+if os.path.isfile(originalFile):
+	if not os.path.isfile(newFile):
+		with open(originalFile, "r") as of, open(newFile, "w") as nf:
+			for line in of:
+				newLine = replace(line)
+				nf.write(newLine)
+
+
+	else:
+		print("Error: The new file already exists")
+		exit(1)
+else:
+	print("Error: The original file not found")
+	exit(1)
